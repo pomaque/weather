cbb43a695ab79317451d10f96a4eb5dafd56cae8
emirkarakavak <92368795+emirkarakavak@users.noreply.github.com>
2022-07-20 11:41:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const request = require("request");
+const axios = require("axios");
 const express = require("express");
 const mongoose = require("mongoose");
+const moment = require('moment-timezone');
 const cron = require("node-cron");
 const app = express();
+
+const dateTurkey = moment.tz("Europe/Istanbul").format();
 
 mongoose.connect("mongodb://localhost:27017/TempDB", (err) => {
   if (err) console.log(err);
@@ -15,14 +19,15 @@ const TempSchema = new mongoose.Schema({
   norMax: { type: Number },
   absMin: { type: Number },
   absMax: { type: Number },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: String, default: dateTurkey },
 });
 const Temps = mongoose.model("Temp", TempSchema);
 //#endregion
 
 //#region Cities
-const izmir = "http://localhost:3000/test";
-//https://tr.weatherspark.com/td/94320/%C4%B0zmir-T%C3%BCrkiye-Ortalama-Hava-Durumu-Bug%C3%BCn
+const izmir = 
+  "https://tr.weatherspark.com/td/94320/%C4%B0zmir-T%C3%BCrkiye-Ortalama-Hava-Durumu-Bug%C3%BCn" //"http://localhost:3000/test";
+
 const istanbul =
   "https://tr.weatherspark.com/td/95434/%C4%B0stanbul-T%C3%BCrkiye-Ortalama-Hava-Durumu-Bug%C3%BCn";
 const ankara =
@@ -31,12 +36,12 @@ const bursa =
   "https://tr.weatherspark.com/td/96052/Bursa-T%C3%BCrkiye-Ortalama-Hava-Durumu-Bug%C3%BCn";
 //#endregion
 
-cron.schedule("*/1 * * * *", () => {
+// cron.schedule("*/1 * * * *", () => {
   getDegree(izmir);
   getDegree(istanbul);
   getDegree(ankara);
   getDegree(bursa);
-});
+// });
 
 function getDegree(cityURL) {
   let randomHeaders = ["Emre", "Erbek", "Adamdir"];
@@ -62,27 +67,28 @@ function getDegree(cityURL) {
           temps.push(fullTextSplit[i].replace("°C", "") * 1);
         }
       }
+Temps.create({
+          city: fullTextSplit[6],
+          norMin: temps[0],
+          norMax: temps[1],
+          absMin: temps[2],
+          absMax: temps[3],
+        })
 
-      Temps.create({
-        city: fullTextSplit[6],
-        norMin: temps[0],
-        norMax: temps[1],
-        absMin: temps[2],
-        absMax: temps[3],
-      });
 
-      console.log(fullTextSplit[6] + " " + temps);
+console.log(fullTextSplit[6] + " " + temps);
     }
   );
+  console.log(dateTurkey)
   // process.exit();
 }
 
-//Header test
-app.get("/test", function (req, res, next) {
-  console.log(req.headers);
-});
+// //Header test
+// app.get("/test", function (req, res, next) {
+//   console.log(req.headers);
+// });
 
-var port = 3000
-app.listen(port, ()=>{
-    console.log(`Server is running on ${port}`)
-});
+// var port = 3000
+// app.listen(port, ()=>{
+//     console.log(`Server is running on ${port}`)
+// });
